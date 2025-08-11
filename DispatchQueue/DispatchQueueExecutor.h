267bf8f311d2e3acfaa6b361e13c134e361900b1
ratlx <louxu@hnu.edu.cn>
2025-08-11 12:46:50 +0800

    use a dynamic queueIdQueue

--- DispatchQueue/DispatchQueueExecutor.h
+++ DispatchQueue/DispatchQueueExecutor.h
@@ -5,9 +5,9 @@
 #pragma once
 
 #include <chrono>
-#include <limits>
 #include <mutex>
-#include <optional>
+#include <queue>
+#include <semaphore>
 #include <set>
 #include <shared_mutex>
 #include <thread>
@@ -16,12 +16,15 @@
 #include "DispatchKeepAlive.h"
 #include "DispatchQueue.h"
 #include "DispatchTask.h"
-#include "TaskQueue/MPMCQueue.h"
-#include "TaskQueue/PrioritySemMPMCQueue.h"
+#include "TaskQueue/BlockingQueue.h"
 #include "Utility.h"
 
 namespace detail {
+using ExecutorKA = DispatchKeepAlive::KeepAlive<DispatchQueueExecutor>;
+
 class DispatchQueueExecutor : public detail::DispatchKeepAlive {
+  enum { kDefaultPriority = 3, kMaxQueueSize = 100000000 };
+
  public:
   struct Thread {
     explicit Thread() : id(nextId.fetch_add(1)), handle() {}
@@ -38,31 +41,59 @@ class DispatchQueueExecutor : public detail::DispatchKeepAlive {
 
   using ThreadPtr = std::shared_ptr<Thread>;
   using ThreadList = std::set<ThreadPtr>;
-  using StoppedThreadQueue = MPMCQueue<ThreadPtr>;
+
+  class StoppedThreadQueue {
+   public:
+    void emplace(ThreadPtr&& thread) {
+      std::lock_guard lock{mutex_};
+      queue_.emplace(std::move(thread));
+      sem_.release();
+    }
+
+    ThreadPtr take() {
+      while (true) {
+        {
+          std::lock_guard lock{mutex_};
+          if (!queue_.empty()) {
+            auto ptr = std::move(queue_.front());
+            queue_.pop();
+            return ptr;
+          }
+        }
+        sem_.acquire();
+      }
+    }
+
+   private:
+    std::queue<ThreadPtr> queue_;
+    std::mutex mutex_;
+    std::counting_semaphore<> sem_{0};
+  };
 
   DispatchQueueExecutor(
       size_t numThreads, uint8_t numPriorities, size_t maxQueueSize);
+
+  DispatchQueueExecutor(
+      size_t numThreads, uint8_t numPriorities = kDefaultPriority);
 
   ~DispatchQueueExecutor();
 
   void add(size_t queueId) { addWithPriority(queueId, Priority::MID_PRI); }
   void addWithPriority(size_t queueId, int8_t priority);
 
-  uint8_t getNumPriorities() const { return queueIdQueue_.getNumPriorities(); }
+  uint8_t getNumPriorities() const { return queueIdQueue_->getNumPriorities(); }
 
   size_t getQueueSize() const noexcept;
 
   void stop();
   void join();
 
-  static DispatchKeepAlive::KeepAlive<DispatchQueueExecutor>
-  getGlobalExecutor();
+  static ExecutorKA getGlobalExecutor();
 
-  using DispatchQueueList =
-      std::vector<DispatchKeepAlive::KeepAlive<DispatchQueue>>;
+  using DispatchQueueList = std::vector<detail::QueueKA>;
   size_t registerDispatchQueue(DispatchQueue*);
   void deregisterDispatchQueue(DispatchQueue*);
-  DispatchKeepAlive::KeepAlive<DispatchQueue> getQueueToken(size_t id);
+  detail::QueueKA getQueueToken(size_t id);
 
  private:
   void ensureJoined();
@@ -73,10 +104,10 @@ class DispatchQueueExecutor : public detail::DispatchKeepAlive {
   void stopThreads(size_t n);
   void stopAndJoinAllThreads(bool isJoin);
 
-  std::optional<detail::DispatchTask> takeNextTask(size_t& queueId);
+  std::pair<detail::QueueKA, detail::DispatchTask> takeNextTask(size_t&);
   bool tryDecrToStop();
-  bool tryThreadTimeout();
-  bool threadShouldStop(const std::optional<detail::DispatchTask>&);
+  bool tryTimeoutThread();
+  bool threadShouldStop();
   void threadRun(ThreadPtr thread);
 
   bool minActive() const noexcept;
@@ -88,8 +119,7 @@ class DispatchQueueExecutor : public detail::DispatchKeepAlive {
 
   std::shared_mutex dispatchQueueLock_;
   // an empty keepAlive occupies index 0
-  DispatchQueueList dispatchQueueList_{
-      DispatchKeepAlive::KeepAlive<DispatchQueue>()};
+  DispatchQueueList dispatchQueueList_{nullptr};
 
   // These are only modified while holding threadListLock_, but
   // are read without holding the lock.
@@ -101,7 +131,7 @@ class DispatchQueueExecutor : public detail::DispatchKeepAlive {
   std::atomic<ssize_t> threadsToStop_{0};
   std::atomic<std::chrono::milliseconds> threadTimeout_;
 
-  PrioritySemMPMCQueue<size_t, QueueBehaviorIfFull::BLOCK> queueIdQueue_;
+  std::unique_ptr<BlockingQueue<size_t>> queueIdQueue_;
 
   bool isJoin_{false};
 };
--- DispatchQueue/DispatchSerialQueue.h
+++ DispatchQueue/DispatchSerialQueue.h
@@ -103,7 +103,7 @@ class DispatchSerialQueue : public DispatchQueue {
   std::queue<detail::DispatchTask> taskQueue_;
   std::mutex taskQueueLock_;
 
-  DispatchKeepAlive::KeepAlive<detail::DispatchQueueExecutor> executor_{};
+  detail::ExecutorKA executor_{};
 
   std::atomic<bool> threadAttach_{false};
 };
