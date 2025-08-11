45267b4b6c63f49f8a1cdda4ec752f955c87e945
ratlx <louxu@hnu.edu.cn>
2025-08-11 11:01:57 +0800

    adapt to the new version of the workitem

--- DispatchQueue/DispatchQueue.h
+++ DispatchQueue/DispatchQueue.h
@@ -18,29 +18,30 @@ struct DispatchAttribute {
   static constexpr uint8_t initiallyInactive = 1 << 2;
 };
 
-class DispatchWorkItem;
 class DispatchGroup;
+class DispatchQueue;
 
 namespace detail {
 class DispatchTask;
+
 class DispatchQueueExecutor;
-struct DispatchQueueAddResult {
-  DispatchQueueAddResult(bool add) : notifiable(add) {}
-  bool notifiable;
-};
+
+class DispatchWorkItemBase;
+
+template <typename T>
+class DispatchNotify;
+
+using QueueKA = DispatchKeepAlive::KeepAlive<DispatchQueue>;
 } // namespace detail
-// namespace detail
 
 class DispatchQueue : public detail::DispatchKeepAlive {
  public:
   virtual ~DispatchQueue() noexcept = default;
 
   virtual void sync(Func<void> func) = 0;
-  virtual void sync(DispatchWorkItem& workItem) = 0;
 
   virtual void async(Func<void> func) = 0;
   virtual void async(Func<void> func, DispatchGroup& group) = 0;
-  virtual void async(DispatchWorkItem& workItem) = 0;
 
   virtual void activate() = 0;
   virtual void suspend() = 0;
@@ -57,10 +58,15 @@ class DispatchQueue : public detail::DispatchKeepAlive {
   std::string getLabel() const noexcept { return label_; }
 
  protected:
+  // The method doNotify in DispatchNotify<void> requires asyncImpl
+  friend class detail::DispatchNotify<void>;
+
+  virtual void asyncImpl(detail::DispatchWorkItemBase*) = 0;
+
   DispatchQueue(std::string label, int8_t priority, uint8_t attributes)
       : label_(std::move(label)), priority_(priority), attribute_(attributes) {
     if (attribute_ & DispatchAttribute::initiallyInactive) {
-      isInactive_ = true;
+      inactive_ = true;
     }
   }
 
@@ -70,7 +76,7 @@ class DispatchQueue : public detail::DispatchKeepAlive {
   std::string label_;
   size_t id_{0};
   const int8_t priority_{0};
-  std::atomic<bool> isInactive_{false};
+  std::atomic<bool> inactive_{false};
   std::atomic<ssize_t> suspendCount_{0};
 
  private:
