6b8495278faeae30dd8b617d00d26b26f8d57d08
ratlx <louxu@hnu.edu.cn>
2025-08-22 00:10:54 +0800

    cancel test in workItem

--- DispatchQueue/tests/WorkItemTest.cpp
+++ DispatchQueue/tests/WorkItemTest.cpp
@@ -37,13 +37,17 @@ TEST(WorkItemTest, Wait) {
   // sync
   DispatchWorkItem<void> w1(add);
   auto sq = DispatchSerialQueue("sq");
+  EXPECT_FALSE(w1.tryWait(10ms));
   sq.sync(w1);
   w1.wait();
+  EXPECT_THROW(w1.perform(), runtime_error);
+  EXPECT_THROW(w1.wait(), runtime_error);
 
   // async
   DispatchWorkItem<void> w2(add);
   sq.async(w2);
-  w2.wait();
+  auto t = thread([&] { w2.wait(); });
+  t.join();
 
   EXPECT_EQ(cnt, 2);
 }
@@ -79,17 +83,33 @@ TEST(WorkItemTest, Notify) {
   sem.acquire();
 }
 
-struct NoCopyOrMoveType {
-  NoCopyOrMoveType() = default;
-  NoCopyOrMoveType(const NoCopyOrMoveType&) noexcept = default;
-  NoCopyOrMoveType(NoCopyOrMoveType&&) noexcept = default;
+TEST(WorkItemTest, Cancel) {
+  int cnt = 0;
+  auto w1 = DispatchWorkItem<int>([&]() -> int { return ++cnt; });
+
+  w1.perform();
+  EXPECT_EQ(cnt, 1);
+
+  Callback<int> c = [](int) { FAIL() << "notify never reach after canceled"; };
+  auto q = DispatchSerialQueue("q");
+
+  w1.notify(q, c);
+  w1.cancel();
+  w1.perform();
+  EXPECT_EQ(cnt, 1);
+}
+
+struct CopyAndMoveType {
+  CopyAndMoveType() = default;
+  CopyAndMoveType(const CopyAndMoveType&) noexcept = default;
+  CopyAndMoveType(CopyAndMoveType&&) noexcept = default;
 };
 
-TEST(WorkItemTest, MoveOnlyType) {
-  DispatchWorkItem<NoCopyOrMoveType> w1([&] { return NoCopyOrMoveType{}; });
+TEST(WorkItemTest, CopyAndMove) {
+  DispatchWorkItem<CopyAndMoveType> w1([&] { return CopyAndMoveType{}; });
   DispatchSerialQueue sq("s1");
   binary_semaphore wait{0};
-  w1.notify(sq, [&](NoCopyOrMoveType) { wait.release(); });
+  w1.notify(sq, [&](CopyAndMoveType) { wait.release(); });
 
   w1.perform();
   wait.acquire();
