6b010c9864a1a4c1768f49b127011725ac296e72
ratlx <louxu@hnu.edu.cn>
2025-08-27 15:34:13 +0800

    add bench

--- DispatchQueue/TaskQueue/tests/MPMCQueueBenchmark.cpp
+++ DispatchQueue/TaskQueue/tests/MPMCQueueBenchmark.cpp
@@ -7,13 +7,15 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
+#include <semaphore>
 
 #include "../MPMCQueue.h"
 
 using namespace std;
 
 constexpr int num_opts = 10000000;
-constexpr int queue_capacity = 100000;
+constexpr int queue_capacity = 1000000;
 int num_threads = 1;
 
 int iterations() {
@@ -22,6 +24,35 @@ int iterations() {
 
 struct BigStruct {
   char pad[192];
+};
+
+template<typename T>
+class MutexQueue {
+public:
+  void push(const T& e) {
+    lock_guard l{m_};
+    q_.push(e);
+    s_.release();
+  }
+
+  void pop(T& t) {
+    while (true) {
+      {
+        lock_guard l{m_};
+        if (!q_.empty()) {
+          t = std::move(q_.front());
+          q_.pop();
+          return;
+        }
+      }
+      s_.acquire();
+    }
+  }
+
+private:
+  mutex m_;
+  queue<T> q_{};
+  counting_semaphore<> s_{0};
 };
 
 template <
@@ -86,6 +117,7 @@ void run_benchmark_for_type(const std::string& type_name) {
   cout << "value type: " << type_name << " (" << sizeof(T) << " bytes)\n\n";
   MPMCQueue<T> q1(queue_capacity);
   MPMCQueue<T, true> q2(queue_capacity);
+  MutexQueue<T> q3;
 
   while (num_threads <= thread::hardware_concurrency()) {
     cout << num_threads << " threads enqueue, " << num_threads
@@ -102,6 +134,12 @@ void run_benchmark_for_type(const std::string& type_name) {
         q2,
         [](auto& q, auto& val) { q.blockingWrite(val); },
         [](auto& q, auto& out) { q.blockingRead(out); });
+
+    benchmark<T>(
+        "MutexQueue",
+        q3,
+        [](auto& q, auto& val) { q.push(val); },
+        [](auto& q, auto& out) { q.pop(out); });
 
     num_threads *= 2;
 
